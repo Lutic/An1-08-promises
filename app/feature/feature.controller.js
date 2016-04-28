26a42e64afc48c1df0134f937c959e2229ff397b
Vitaliy Zhyrytskyy <vitaliy.zhyrytskyy@epam.com>
2016-04-28 09:13:12 +0300

    Start Point

---
+++ app/feature/feature.controller.js
@@ -0,0 +1,14 @@
+(function() {
+	"use strict";
+
+	angular
+		.module("feature")
+		.controller("Feature", Feature);
+
+	function Feature($scope, $q, tasksDataService) {
+
+
+		
+	}
+	
+})();
---
+++ app/feature/feature.service.js
@@ -0,0 +1,14 @@
+(function() {
+	"use strict";
+
+	angular
+		.module("feature")
+		.factory("tasksDataService", tasksDataService);
+
+	function tasksDataService() {
+		return {
+
+		};
+	}
+	
+})();
