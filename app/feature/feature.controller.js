0a5846f095914eae27034fbec176c6aada490b2f
Elya Mikailova <elutic@gmail.com>
2016-11-21 16:13:22 +0300

    promises

--- app/feature/feature.controller.js
+++ app/feature/feature.controller.js
@@ -7,7 +7,56 @@
 
 	function Feature($q, tasksDataService) {
 
+		let $ctrl = this;
 
+		// tasksDataService.getTasksNew()
+		// 	// .then(getTasksSuccess, getTasksError, getTasksNotify);
+		// 	.then(getTasksSuccess)
+		// 	.then(getOtherTask)
+		// 	.catch( getTasksError)
+		// 	.finally(getTasksComplete);
+        //
+		// tasksDataService.getUsers()
+		// 	.then(getUsersSuccess);
+
+		let tasksPromise = tasksDataService.getTasksNew(),
+			usersPromise = tasksDataService.getUsers();
+
+		$q.all([tasksPromise, usersPromise, $q.when(20)])
+			.then(getAllDataSuccess)
+			.catch( getTasksError)
+			.finally(getTasksComplete);
+
+		function getAllDataSuccess(dataArray) {
+			$ctrl.tasks = dataArray[0];
+			$ctrl.users = dataArray[1];
+			console.log(dataArray[2]);
+		}
+		
+		function getUsersSuccess (users) {
+			$ctrl.users = users;
+		}
+
+		function getTasksSuccess(tasks) {
+			$ctrl.tasks = tasks;
+			return 10;
+		}
+
+		function getOtherTask(data) {
+			console.log('DATA', data);
+		}
+
+		function getTasksError(reason) {
+			console.log(reason);
+		}
+
+		function getTasksNotify(msg) {
+			console.log(msg);
+		}
+
+		function getTasksComplete() {
+			console.log("Get Tasks is Completed");
+		}
 		
 	}
 	
--- app/feature/feature.service.js
+++ app/feature/feature.service.js
@@ -6,9 +6,65 @@
 		.factory("tasksDataService", tasksDataService);
 
 	function tasksDataService($q, $timeout) {
-		return {
+		let tasks = [
+			{"id": "1", "action": "Estimate", "description": "Description of the task", "done": "false", "userId": "1", "userName": "Alan", "deadline": "2015-05-30", "priority": "1", "estimation": "3"},
+			{"id": "2", "action": "Create...", "done": "false", "userId": "2", "userName": "Bella"},
+			{"id": "3", "action": "Update...", "done": "false", "userId": "3", "userName": "Vardan"},
+			{"id": "4", "action": "Delete...", "done": "false", "userId": "4", "userName": "Galiia"},
+			{"id": "5", "action": "Deploy", "done": "false", "userId": "5", "userName": "Dmitriy"}
+		];
 
+		let users = [
+			{"name": "Ann June"},
+			{"name": "Ban Gule"},
+			{"name": "Cider Duren"}
+		];
+
+		return {
+			getTasks,
+			getUsers,
+			getTasksNew
 		};
+
+		function getTasks() { // используя deffered объект
+			let def = $q.defer();
+
+			$timeout(() => {
+				let flag = true;
+
+				if(flag) {
+					def.notify("Start...");
+					def.notify("Finish...");
+					def.resolve(tasks);
+				} else {
+					def.reject("Error Tasks");
+				}
+			}, 200);
+
+			return def.promise;
+		};
+
+		function getTasksNew() { // используя конструктор
+			return $q((resolve, reject) => {
+				$timeout(() => {
+					let flag = true;
+
+					flag ? resolve(tasks) : reject("Error Tasks");
+
+				}, 200);
+			});
+		}
+
+		function getUsers() { // используя конструктор
+			return $q((resolve, reject) => {
+				$timeout(() => {
+					let flag = true;
+
+					flag ? resolve(users) : reject("Error Users");
+
+				}, 2000);
+			});
+		}
 	}
 	
 })();
